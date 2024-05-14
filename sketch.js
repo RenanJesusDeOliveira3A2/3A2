887eae29bed7b63969a666137b19af47d1bba232
Renan Jesus De Oliveira-3A2 <valdenicej45@gmail.com>
2024-05-14 11:50:56 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,32 @@
+let cor;
+let ellipseX; // horizontal
+let ellipseY; // vertical
 
+function setup() {
+  createCanvas(400, 400);
+  background(color("yellow"));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  
+  ellipseX = [0, 0, 0, 0];
+  ellipseY = [random(height), random(height), random(height)];
+}
+
+function draw() {
+  
+  fill(cor);
+  
+  for(let contador in ellipseX) {
+    ellipse(ellipseX[contador], ellipseY[contador], 20, 50, 30, 40);    
+    ellipseX[contador]+= random(0,3);
+    ellipseY[contador]+= random(-3,3); 
+    
+    if(ellipseX[contador] >= width){
+      ellipseX[contador] = 0;
+      ellipseY[contador] = random(height);
+    }
+  }
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
